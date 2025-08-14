a3f1e7989168a9d9303bfa04f09950598f19506c
Wogari-GH6877 <wogariararsa0968@gmail.com>
2025-08-14 14:53:25 -0700

    folder change

--- netflix-clone/src/utils/request.js
+++ netflix-clone/src/utils/request.js
@@ -2,6 +2,7 @@ const API_KEY=process.env.REACT_APP_API_KEY;
 
 const request={
     fetchingTrending:`/trending/all/week?api_key={API_KEY}&language=en-US`,
+    fetchingUpComingMovie:`/movie/upcoming?api_key={API_KEY}&language=en-US&page=1`,
     fetchingTopRated:`/movie/top_rated/?api_key={API_KEY}&language=en-US`,
     fetchingActionMovies:`/genre/movie/list?api_key={API_KEY}&genres=28`,
     fetchingComdeyMovies:`/genre/movie/list?api_key={API_KEY}&genres=35`,
