40b6880a78cdc552da0e65de0800951493fb90dc
Wogari-GH6877 <wogariararsa0968@gmail.com>
2025-08-14 14:40:02 -0700

    axios and request

---
+++ netflix-clone/public/utils/request.js
@@ -0,0 +1,14 @@
+const API_KEY=process.env.REACT_APP_API_KEY;
+
+const request={
+    fetchingTrending:`/trending/all/week?api_key={API_KEY}&language=en-US`,
+    fetchingTopRated:`/movie/top_rated/?api_key={API_KEY}&language=en-US`,
+    fetchingActionMovies:`/genre/movie/list?api_key={API_KEY}&genres=28`,
+    fetchingComdeyMovies:`/genre/movie/list?api_key={API_KEY}&genres=35`,
+    fetchingHorrorMovies:`/genre/movie/list?api_key={API_KEY}&genres=27`,
+    fetchingRomanceMovies:`/genre/movie/list?api_key={API_KEY}&genres=10749`,
+    fetchingDocumentryMovies:`/genre/movie/list?api_key={API_KEY}&genres=99`,
+    fetchingTvShow:`/tv/popular?api_key={API_KEY}&language=en-US&page=1`,
+};
+
+export default request;
